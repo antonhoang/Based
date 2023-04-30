e22a174f14645ca6d693d4e7ed8f64be2244882f
antuan.khoanh <antonhoang@antons-macbook-pro.home>
2023-04-30 19:02:08 +0200

    add networkmanager with unit tests

---
+++ Based/Service/Network/NetworkManager.swift
@@ -0,0 +1,73 @@
+//
+//  NetworkManager.swift
+//  Based
+//
+//  Created by antuan.khoanh on 30/04/2023.
+//
+
+import Foundation
+
+typealias NetworkCompletion<T> = (Result<T, Error>) -> Void
+
+class NetworkManager {
+    
+    private let session: URLSession
+    private var cache: NSCache<NSURL, NSData>
+    private var tasks: [URL: URLSessionDataTask] = [:]
+    
+    init(session: URLSession = .shared, cache: NSCache<NSURL, NSData> = NSCache<NSURL, NSData>()) {
+        self.session = session
+        self.cache = cache
+    }
+    
+    func fetchData<T: Decodable>(from url: URL, completion: @escaping NetworkCompletion<T>) {
+        
+        if let cachedData = cache.object(forKey: url as NSURL) {
+            if let decodedData = try? JSONDecoder().decode(T.self, from: cachedData as Data) {
+                completion(.success(decodedData))
+                return
+            }
+        }
+        
+        let task = session.dataTask(with: url) { [weak self] data, response, error in
+            
+            guard let self = self else { return }
+            
+            self.tasks[url] = nil
+            
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                let error = NSError(domain: "NetworkError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid HTTP response"])
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "NetworkError", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])
+                completion(.failure(error))
+                return
+            }
+            
+            if let decodedData = try? JSONDecoder().decode(T.self, from: data) {
+                self.cache.setObject(data as NSData, forKey: url as NSURL)
+                completion(.success(decodedData))
+            } else {
+                let error = NSError(domain: "NetworkError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unable to decode data"])
+                completion(.failure(error))
+            }
+        }
+        
+        tasks[url] = task
+        task.resume()
+    }
+    
+    func cancelTask(for url: URL) {
+        tasks[url]?.cancel()
+        tasks[url] = nil
+    }
+}
---
+++ BasedTests/NetworkManagerTests.swift
@@ -0,0 +1,88 @@
+//
+//  BasedTests.swift
+//  BasedTests
+//
+//  Created by antuan.khoanh on 30/04/2023.
+//
+
+import XCTest
+@testable import Based
+
+final class NetworkManagerTests: XCTestCase {
+    
+    var session: URLSession!
+    var cache: NSCache<NSURL, NSData>!
+    var networkManager: NetworkManager!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        session = URLSession(configuration: .default)
+        cache = NSCache<NSURL, NSData>()
+        networkManager = NetworkManager(session: session, cache: cache)
+    }
+
+    override func tearDownWithError() throws {
+        session = nil
+        cache = nil
+        networkManager = nil
+        try super.tearDownWithError()
+    }
+    
+    func testFetchDataSuccess() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+        let expectation = XCTestExpectation(description: "Fetch data completes successfully")
+        networkManager.fetchData(from: url, completion: { (result: Result<TodoMock, Error>) in
+            switch result {
+            case .success(let todo):
+                XCTAssertEqual(todo.id, 1)
+                XCTAssertEqual(todo.title, "delectus aut autem")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Fetch data failed with error: \(error)")
+            }
+        })
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func testFetchDataFailure() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/invalid")!
+        let expectation = XCTestExpectation(description: "Fetch data fails")
+        networkManager.fetchData(from: url) { (result: Result<TodoMock, Error>) in
+            switch result {
+            case .success:
+                XCTFail("Fetch data succeeded when it should have failed")
+            case .failure:
+                expectation.fulfill()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func testFetchDataCancels() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+        let expectation = XCTestExpectation(description: "Fetch data cancels")
+        networkManager.fetchData(from: url) { (result: Result<TodoMock, Error>) in
+            switch result {
+            case .success:
+                XCTFail("Fetch data succeeded when it should have been cancelled")
+            case .failure(let error):
+                XCTAssertEqual(error.localizedDescription, "cancelled")
+                expectation.fulfill()
+            }
+        }
+        networkManager.cancelTask(for: url)
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    
+    struct TodoMock: Codable {
+        let userId: Int
+        let id: Int
+        let title: String
+        let completed: Bool
+    }
+
+
+}
